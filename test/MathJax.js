00061648bcea4c7d3c2d264aef012b730dd608f0
Peter Krautzberger <p.krautzberger@gmail.com>
2017-05-02 17:29:26 +0200

    test fix for old browsers

--- test/MathJax.js
+++ test/MathJax.js
@@ -1,6 +1,6 @@
 (function () {
   var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js';
-  var oldMathJax = 'rawgit.com/mathjax/cdn-redirect/master/test/MathJax.js';
+  var oldMathJax = 'rawgit.com/mathjax/cdn-redirect/1741/test/MathJax.js';
   var n = oldMathJax.length;
 
   var replaceScript = function (script, rawSrc) {
@@ -16,7 +16,7 @@
     var scripts = document.getElementsByTagName('script');
     for (var i = 0; i < scripts.length; i++) {
       var script = scripts[i];
-      var src = script.getAttribute('src') || '//';
+      var src = script.src || '//';
       var rawSrc = src.split('//')[1];
       if (rawSrc.substr(0, n) === oldMathJax) {
         replaceScript(script, rawSrc);
